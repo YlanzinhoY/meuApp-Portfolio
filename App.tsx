9173d202be57fcaf3bbaf316a0230a3630fd2046
enzinho <enzo.maxpayne@gmail.com>
2023-01-31 17:46:59 -0300

    removendo arquivo .txt

--- App.tsx
+++ App.tsx
@@ -3,15 +3,8 @@ import * as React from 'react'
 import { Competencias} from "./src/screens/Competencias/index";
 import { Main } from './src/screens/Main/index'
 
-
-
 import { ThemeProvider } from "styled-components";
 import theme from "./src/theme/index";
-
-
-
-
-
 
 
 export default function App() {
